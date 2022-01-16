fa5cf170fa409c1ed052f0e756437452bd7fc9e2
yalin01234 <45939336+yalin01234@users.noreply.github.com>
2022-01-16 01:37:32 -0500

    Update Books_jsp.java

--- BookStoreJava_21403_lines/Books_jsp.java
+++ BookStoreJava_21403_lines/Books_jsp.java
@@ -16,9 +16,9 @@ public class Books_jsp extends HttpJspBase {
 
   static final int UNDEFINT=Integer.MIN_VALUE;
 
-  static final int adText = 1;
-  static final int adDate = 2;
-  static final int adNumber = 3;
+  static final int adText = 2;
+  static final int adDate = 3;
+  static final int adNumber = 4;
   static final int adSearch_ = 4;
   static final int ad_Search_ = 5;
   static final String appPath   ="/";
